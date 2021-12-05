e697c16355df30c875d831f44247e9911e43bab3
Lucas santos <lucas.nepomuceno1999@gmail.com>
2021-12-05 01:45:08 -0300

    added fake unit test to exemple

--- app/src/androidTest/java/com/lnsantos/ci_cdandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lnsantos/ci_cdandroid/ExampleInstrumentedTest.kt
@@ -15,10 +15,62 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.lnsantos.ci_cdandroid", appContext.packageName)
     }
+
+    @Test
+    fun test_1(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_2(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_3(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_4(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_5(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_6(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_7(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_8(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_9(){
+        assert(value = true)
+    }
+
+    @Test
+    fun test_10(){
+        assert(value = true)
+    }
+
 }
